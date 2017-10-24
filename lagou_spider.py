6ee92400814cf0342bf978c78e821678251ea19f
tusonggao <tusonggao@163.com>
2017-10-24 22:54:19 +0800

    add file

---
+++ lagou_spider.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup 
+import urllib
+import re
+import time
+import os
+import sip
+import PyQt4
+import spynner
+from collections import OrderedDict
+import requests
+
+crawled_urls_set = set()
+
+def make_dir(path_dir):
+    cwd_path = os.getcwd()
+    path = os.path.join(cwd_path, path_dir)
+    if not os.path.exists(path):
+        os.mkdir(path)                 #创建文件夹
+
+def store_file(url_set, path):
+    global headers, crawled_urls_set
+    
+    for url in url_set:
+        print('crawling url ', url)
+        if url in crawled_urls_set:
+            print('already crawled url ', url)
+            continue
+        else:
+            crawled_urls_set.add(url)
+        file_number = url.split('/')[-1].split('.')[0]
+        filename = path + os.sep + str(file_number) + '.html'
+        try:
+            content = requests.get(url, headers=headers, timeout=45).text
+        except Exception as e:
+            print('error occured ', str(e))
+        else:
+            with open(filename, 'wb') as f:
+                f.write(content)
+                
+        
+def get_job_link(html):
+    soup = BeautifulSoup(html, 'lxml')
+    positions = soup.find_all(class_='position_link')
+    link_list = [pos['href'] for pos in positions]
+    return link_list
+    
+
+def get_job_urls(start_url):
+    driver = webdriver.Firefox()
+    driver.get(start_url)
+    WebDriverWait(driver, 30)
+    
+    position_link_set = set()
+    
+    count = 0
+    while count<=30:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);");  # 下拉到底部
+        try:
+            next_page = driver.find_element_by_class_name('pager_next')
+            next_page.click()
+        except Exception as e:
+            print('Exception is ', str(e))
+        time.sleep(1)
+        
+        link_list = get_job_link(driver.page_source)
+        position_link_set |= set(link_list)
+        print('url_list_set length is {0} '.format(len(position_link_set)))
+        count += 1
+        
+    return position_link_set
+
+
+if __name__=='__main__':
+    start_url = 'https://www.lagou.com/jobs/list_%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0' # 减肥照片
+    headers = {
+        'origin':
+        "https://www.lagou.com",
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.76 Safari/537.36'
+    }
+         
+#    headers = {
+#        'origin':
+#        "https://www.lagou.com",
+#        'x-anit-forge-code':
+#        "0",
+#        'user-agent':
+#        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36",
+#        'content-type':
+#        "application/x-www-form-urlencoded",
+#        'accept':
+#        "application/json, text/javascript, */*; q=0.01",
+#        'x-requested-with':
+#        "XMLHttpRequest",
+#        'x-anit-forge-token':
+#        "None",
+#        'dnt':
+#        "1",
+#        'accept-encoding':
+#        "gzip, deflate, br",
+#        'accept-language':
+#        "zh-CN,zh;q=0.8,en;q=0.6",
+#        'cookie':
+#        "user_trace_token=20170728162449-adcee15cc85848189cfb891619b80998; LGUID=20170728162451-3a327df6-736e-11e7-b9bc-5254005c3644; _gat=1; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fwww.lagou.com%2Fgongsi%2F; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; LGSID=20170807221703-15e8d3a5-7b7b-11e7-839b-5254005c3644; index_location_city=%E5%85%A8%E5%9B%BD; JSESSIONID=ABAAABAACBHABBI1DE966CF9357E8BD2D82C53257584955; X_HTTP_TOKEN=2274185a2011ec03f73ab98f8ceaf490; TG-TRACK-CODE=index_navigation; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1501230293; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1503803797; _ga=GA1.2.1475031428.1501230295; LGRID=20170827111636-22b244c2-8ad6-11e7-8f3c-5254005c3644; SEARCH_ID=52d0e756ac5d41b395d4a9e57ab74b72; user_trace_token=20170728162449-adcee15cc85848189cfb891619b80998; LGUID=20170728162451-3a327df6-736e-11e7-b9bc-5254005c3644; index_location_city=%E5%85%A8%E5%9B%BD; user_trace_token=20170728162449-adcee15cc85848189cfb891619b80998; LGUID=20170728162451-3a327df6-736e-11e7-b9bc-5254005c3644; _gat=1; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fwww.lagou.com%2Fgongsi%2F; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; LGSID=20170807221703-15e8d3a5-7b7b-11e7-839b-5254005c3644; index_location_city=%E5%85%A8%E5%9B%BD; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1501230293; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1503587852; _ga=GA1.2.1475031428.1501230295; LGRID=20170824231731-59096c0b-88df-11e7-8ea0-5254005c3644; JSESSIONID=ABAAABAACBHABBI1DE966CF9357E8BD2D82C53257584955; X_HTTP_TOKEN=2274185a2011ec03f73ab98f8ceaf490; TG-TRACK-CODE=index_navigation; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1501230293; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1503807054; _ga=GA1.2.1475031428.1501230295; LGSID=20170827121054-b83e61a1-8add-11e7-8f3c-5254005c3644; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fwww.lagou.com%2Fzhaopin%2Fheiheceshi%2F%3FlabelWords%3Dlabel; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2Fjobs%2Flist_Go%3Fcity%3D%25E5%2585%25A8%25E5%259B%25BD%26cl%3Dfalse%26fromSearch%3Dtrue%26labelWords%3D%26suginput%3D; LGRID=20170827121054-b83e632d-8add-11e7-8f3c-5254005c3644; SEARCH_ID=ced77b843c064940a3c12c51d8720ca1",
+#    }
+         
+    job_link_set = get_job_urls(start_url)
+    print('len of job_link_set is ', len(job_link_set))
+    store_file(job_link_set, './jobs/')
+    
+    
+#    file_map = {'1.jpg': 'http://baidu.com', '2.jpg': 'http://google.com'}
+#    with open('file_url.txt', 'w') as f:
+#        f.write(str(file_map))
+    
+#    pic_url = 'https://pic3.zhimg.com/v2-ba523260fe45bb5e43a3e129dd83dd4a_r.jpg'
+#    file_name = './zhihu_pic/1.jpg'
+#    try:
+#        content = requests.get(pic_url).content
+#    except Exception as e:
+#        print('error occured ', str(e))
+#    else:
+#        with open(file_name, 'wb') as f:
+#            f.write(content)
+
